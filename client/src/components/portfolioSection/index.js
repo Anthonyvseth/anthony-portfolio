c0e2fc25d8b235e8d44b164e4fe59490cce6f529
Anthonyvseth <62005175+Anthonyvseth@users.noreply.github.com>
2021-02-01 01:20:24 -0700

    4th project added to porfolio

--- client/src/components/portfolioSection/index.js
+++ client/src/components/portfolioSection/index.js
@@ -12,13 +12,14 @@ import {
  import project1 from '../../assets/project1.gif'
  import project2 from '../../assets/project2.gif'
  import project3 from '../../assets/project3.gif'
+ import project4 from '../../assets/project4.gif'
 
 const Portfolio = ({ id }) => {
 
     function gitStudioGhibli() {
         window.open('https://github.com/Anthonyvseth/Studio-Ghibli-API')
       }
-      function StudioGhibli() {
+    function StudioGhibli() {
         window.open('https://anthonyvseth.github.io/Studio-Ghibli-API/index.html')
       }
 
@@ -36,15 +37,33 @@ const Portfolio = ({ id }) => {
         window.open('https://cake-baker-blog.herokuapp.com/')
       }
 
+    function gitLifeLesson() {
+        window.open('https://github.com/Anthonyvseth/React-Redux-Firebase-App')
+      }
+    function lifeLesson() {
+        window.open('https://life-lesson-share-app.web.app/')
+      }
+
     return (
         <PortContainer id={id}>
             <PortH1>Portfolio</PortH1>
             <PortWrapper>
             <PortCard>
                 <ImgWrap>
+                    <Img src={project4}/>
+                </ImgWrap>
+                <PortH2>Life Lessons</PortH2>
+                <PortP>A website for people to share any life lesson they have. Once signed in you can view all of the life lessons that every user has created. A react, redux, firebase application.</PortP>
+                <LinkWrap>
+                    <PortLink onClick={gitLifeLesson}>GitHub</PortLink>
+                    <PortLink onClick={lifeLesson}>Website</PortLink>
+                </LinkWrap>
+            </PortCard>
+            <PortCard>
+                <ImgWrap>
                     <Img src={project3}/>
                 </ImgWrap>
-                <PortH2>Make Your Day</PortH2>
+                <PortH2>Cake Baker Blog</PortH2>
                 <PortP>Created for my partner that runs a small buisness. A protoype website for her to display and give details about each new cake that she creates. MERN fullstack app with full crud.</PortP>
                 <LinkWrap>
                     <PortLink onClick={gitCakeBlog}>GitHub</PortLink>
