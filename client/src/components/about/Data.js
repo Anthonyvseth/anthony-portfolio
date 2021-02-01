106ea0ad1085b1fa03cfd44e87ba38fdd2028d2a
Anthonyvseth <62005175+Anthonyvseth@users.noreply.github.com>
2021-02-01 10:43:12 -0700

    updated about me description

--- client/src/components/about/Data.js
+++ client/src/components/about/Data.js
@@ -1,7 +1,7 @@
 export const aboutObjOne = {
     id: 'about',
     headline: 'Anthony Seth',
-    description:`I am a software engineer driven to create a better a future. My goal is to simplify solutions to life's complex problems and use my continual learning to keep improving the quality and ease of life through software. As we grow, we are always building new skills and abilities. Efficiency and organization are my key skills and having an organized mind, in turn makes it easy to find where there can be any issues in code and in life.`,
+    description:`I am a motivated software engineer with the ability to challenge others and myself to develop solutions to problems and situations. I am driven by growth within this field of work. I have shown strong leadership skills with an understanding of both individual and team goals to collaboratively work towards a common goal. My overall goal as a software engineer is to simplify solutions to technology’s complex problems and use the knowledge I already possess to keep improving the quality and ease of work through software. I also have strong organizational skills that allow efficient progression and division of tasks amongst my peers and myself. My objective is to obtain a position that will allow me to utilize and apply my experiences to achieve any task or goal that is set ahead of me.`,
     buttonLabel: '',
     imgStart: false,
     alt: 'Profile Picture',
