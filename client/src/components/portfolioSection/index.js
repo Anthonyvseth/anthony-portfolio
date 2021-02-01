5e54bd094045b4022b7872f7f5b9ac26f2194ccf
Anthonyvseth <62005175+Anthonyvseth@users.noreply.github.com>
2021-02-01 00:42:25 -0700

    added second project to portfolio

--- client/src/components/portfolioSection/index.js
+++ client/src/components/portfolioSection/index.js
@@ -10,11 +10,22 @@ import {
     PortLink, PortWrapper, LinkWrap
  } from './PortfolioElems'
  import project1 from '../../assets/project1.gif'
+ import project2 from '../../assets/project2.gif'
 
 const Portfolio = ({ id }) => {
 
     function gitStudioGhibli() {
         window.open('https://github.com/Anthonyvseth/Studio-Ghibli-API')
+      }
+      function StudioGhibli() {
+        window.open('https://anthonyvseth.github.io/Studio-Ghibli-API/index.html')
+      }
+
+    function gitMakeYourDay() {
+        window.open('https://github.com/Anthonyvseth/Unit-3-project')
+      }
+    function makeYourDay() {
+        window.open('https://make-your-day-2.herokuapp.com/')
       }
 
     return (
@@ -29,7 +40,18 @@ const Portfolio = ({ id }) => {
                 <PortP>The very first deployed project I have ever created. It is very important to me because it shows this because it shows how much growth I have gained since I began my software engineering journey.</PortP>
                 <LinkWrap>
                     <PortLink onClick={gitStudioGhibli}>GitHub</PortLink>
-                    <PortLink onClick={gitStudioGhibli}>Website</PortLink>
+                    <PortLink onClick={StudioGhibli}>Website</PortLink>
+                </LinkWrap>
+            </PortCard>
+            <PortCard>
+                <ImgWrap>
+                    <Img src={project2}/>
+                </ImgWrap>
+                <PortH2>Make Your Day</PortH2>
+                <PortP>Colaborative project based on google extension called 'Momentun'. My main job within the project was frontend work with react.</PortP>
+                <LinkWrap>
+                    <PortLink onClick={gitMakeYourDay}>GitHub</PortLink>
+                    <PortLink onClick={makeYourDay}>Website</PortLink>
                 </LinkWrap>
             </PortCard>
             </PortWrapper>
